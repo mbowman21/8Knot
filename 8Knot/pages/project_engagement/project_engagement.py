831d40a84e13329a38006f89663bb77c59c731a7
mbowman21 <sstraberry101@gmail.com>
2023-11-28 19:44:07 -0600

    pushing up the initial edits

--- 8Knot/pages/project_engagement/project_engagement.py
+++ 8Knot/pages/project_engagement/project_engagement.py
@@ -3,6 +3,10 @@ import dash
 import dash_bootstrap_components as dbc
 import warnings
 
+from visualizations.issues_closed import gc_issues_closed
+from visualizations.issues_updated import gc_issues_updated
+from visualizations.contributors import gc_contributors
+
 # import visualization cards
 
 warnings.filterwarnings("ignore")
@@ -13,7 +17,15 @@ layout = dbc.Container(
     [
         dbc.Row(
             [
-
+                dbc.Col(gc_issues_closed, width=6),
+                dbc.Col(gc_issues_updated, width=6),
+            ],
+            align="center",
+            style={"marginBottom": ".5%"},
+        ),
+        dbc.Row(
+            [
+                dbc.Col(gc_contributors, width=6),
             ],
             align="center",
             style={"marginBottom": ".5%"},
