b828f419ea95045c34baeb8804284b7a040a6d4f
vile-kreacher <143014049+vile-kreacher@users.noreply.github.com>
2023-11-30 15:16:46 -0600

    Finished time_to_first_response.py

--- 8Knot/pages/project_starter_health/visualizations/time_to_first_response.py
+++ 8Knot/pages/project_starter_health/visualizations/time_to_first_response.py
@@ -8,7 +8,7 @@ import logging
 from dateutil.relativedelta import *  # type: ignore
 import plotly.express as px
 from pages.utils.graph_utils import get_graph_time_values, color_seq
-from queries.contributors_query import contributors_query as ctq
+from queries.pr_response_query import pr_response_query as prr
 import io
 from cache_manager.cache_manager import CacheManager as cm
 from pages.utils.job_utils import nodata_graph
@@ -16,7 +16,6 @@ import time
 import datetime as dt
 import math
 import numpy as np
-
 
 PAGE = "project_starter_health"
 VIZ_ID = "time_to_first_response"
@@ -34,10 +33,9 @@ gc_time_to_first_response = dbc.Card(
                     [
                         dbc.PopoverHeader("Graph Info:"),
                         dbc.PopoverBody(
-                            """This visualization gives a view into the development speed of a repository in\n
-                            relation to the other selected repositories. For more context of this visualization see\n
-                            https://chaoss.community/kb/metric-project-velocity/ \n
-                            https://www.cncf.io/blog/2017/06/05/30-highest-velocity-open-source-projects/ """
+                            """
+                            Shows how many issues are open vs. how many get a response within a given time frame.
+                            """
                         ),
                     ],
                     id=f"popover-{PAGE}-{VIZ_ID}",
@@ -53,131 +51,25 @@ gc_time_to_first_response = dbc.Card(
                         dbc.Row(
                             [
                                 dbc.Label(
-                                    "Issue Opened Weight:",
-                                    html_for=f"issue-opened-weight-{PAGE}-{VIZ_ID}",
-                                    width={"size": "auto"},
-                                ),
-                                dbc.Col(
-                                    dbc.Input(
-                                        id=f"issue-opened-weight-{PAGE}-{VIZ_ID}",
-                                        type="number",
-                                        min=0,
-                                        max=1,
-                                        step=0.1,
-                                        value=0.3,
-                                        size="sm",
-                                    ),
-                                    className="me-2",
-                                    width=1,
-                                ),
-                                dbc.Label(
-                                    "Issue Closed Weight:",
-                                    html_for=f"issue-closed-weight-{PAGE}-{VIZ_ID}",
-                                    width={"size": "auto"},
-                                ),
-                                dbc.Col(
-                                    dbc.Input(
-                                        id=f"issue-closed-weight-{PAGE}-{VIZ_ID}",
-                                        type="number",
-                                        min=0,
-                                        max=1,
-                                        step=0.1,
-                                        value=0.4,
-                                        size="sm",
-                                    ),
-                                    className="me-2",
-                                    width=1,
-                                ),
-                                dbc.Label(
-                                    "Y-axis:",
-                                    html_for=f"graph-view-{PAGE}-{VIZ_ID}",
+                                    "Response Days:",
+                                    html_for=f"response-days-{PAGE}-{VIZ_ID}",
                                     width="auto",
                                 ),
                                 dbc.Col(
-                                    dbc.RadioItems(
-                                        id=f"graph-view-{PAGE}-{VIZ_ID}",
-                                        options=[
-                                            {"label": "Non-log", "value": False},
-                                            {"label": "Log", "value": True},
-                                        ],
-                                        value=False,
-                                        inline=True,
-                                    ),
-                                    className="me-2",
-                                ),
-                            ],
-                            align="center",
-                        ),
-                        dbc.Row(
-                            [
-                                dbc.Label(
-                                    "PR Open Weight:",
-                                    html_for=f"pr-open-weight-{PAGE}-{VIZ_ID}",
-                                    width={"size": "auto"},
-                                ),
-                                dbc.Col(
                                     dbc.Input(
-                                        id=f"pr-open-weight-{PAGE}-{VIZ_ID}",
+                                        id=f"response-days-{PAGE}-{VIZ_ID}",
                                         type="number",
-                                        min=0,
-                                        max=1,
-                                        step=0.1,
-                                        value=0.5,
+                                        min=1,
+                                        max=120,
+                                        step=1,
+                                        value=2,
                                         size="sm",
                                     ),
                                     className="me-2",
-                                    width=1,
-                                ),
-                                dbc.Label(
-                                    "PR Merged Weight:",
-                                    html_for=f"pr-merged-weight-{PAGE}-{VIZ_ID}",
-                                    width={"size": "auto"},
+                                    width=2,
                                 ),
                                 dbc.Col(
-                                    dbc.Input(
-                                        id=f"pr-merged-weight-{PAGE}-{VIZ_ID}",
-                                        type="number",
-                                        min=0,
-                                        max=1,
-                                        step=0.1,
-                                        value=0.7,
-                                        size="sm",
-                                    ),
-                                    className="me-2",
-                                    width=1,
-                                ),
-                                dbc.Label(
-                                    "PR Closed Weight:",
-                                    html_for=f"pr-closed-weight-{PAGE}-{VIZ_ID}",
-                                    width={"size": "auto"},
-                                ),
-                                dbc.Col(
-                                    dbc.Input(
-                                        id=f"pr-closed-weight-{PAGE}-{VIZ_ID}",
-                                        type="number",
-                                        min=0,
-                                        max=1,
-                                        step=0.1,
-                                        value=0.2,
-                                        size="sm",
-                                    ),
-                                    className="me-2",
-                                    width=1,
-                                ),
-                            ],
-                            align="center",
-                        ),
-                        dbc.Row(
-                            [
-                                dbc.Col(
-                                    dcc.DatePickerRange(
-                                        id=f"date-picker-range-{PAGE}-{VIZ_ID}",
-                                        min_date_allowed=dt.date(2005, 1, 1),
-                                        max_date_allowed=dt.date.today(),
-                                        initial_visible_month=dt.date(dt.date.today().year, 1, 1),
-                                        clearable=True,
-                                    ),
-                                    width="auto",
+                                    width=6,
                                 ),
                                 dbc.Col(
                                     dbc.Button(
@@ -213,32 +105,22 @@ def toggle_popover(n, is_open):
     return is_open
 
 
-# callback for Project Velocity graph
+# callback for first response graph
 @callback(
     Output(f"{PAGE}-{VIZ_ID}", "figure"),
     [
         Input("repo-choices", "data"),
-        Input(f"graph-view-{PAGE}-{VIZ_ID}", "value"),
-        Input(f"issue-opened-weight-{PAGE}-{VIZ_ID}", "value"),
-        Input(f"issue-closed-weight-{PAGE}-{VIZ_ID}", "value"),
-        Input(f"pr-open-weight-{PAGE}-{VIZ_ID}", "value"),
-        Input(f"pr-merged-weight-{PAGE}-{VIZ_ID}", "value"),
-        Input(f"pr-closed-weight-{PAGE}-{VIZ_ID}", "value"),
-        Input(f"date-picker-range-{PAGE}-{VIZ_ID}", "start_date"),
-        Input(f"date-picker-range-{PAGE}-{VIZ_ID}", "end_date"),
+        Input(f"response-days-{PAGE}-{VIZ_ID}", "value"),
     ],
     background=True,
 )
-def project_velocity_graph(
-    repolist, log, i_o_weight, i_c_weight, pr_o_weight, pr_m_weight, pr_c_weight, start_date, end_date
-):
-
+def pr_first_response_graph(repolist, num_days):
     # wait for data to asynchronously download and become available.
     cache = cm()
-    df = cache.grabm(func=ctq, repos=repolist)
+    df = cache.grabm(func=prr, repos=repolist)
     while df is None:
         time.sleep(1.0)
-        df = cache.grabm(func=ctq, repos=repolist)
+        df = cache.grabm(func=prr, repos=repolist)
 
     start = time.perf_counter()
     logging.warning(f"{VIZ_ID}- START")
@@ -248,103 +130,125 @@ def project_velocity_graph(
         logging.warning(f"{VIZ_ID} - NO DATA AVAILABLE")
         return nodata_graph
 
-    # function for all data pre processing
-    df = process_data(df, start_date, end_date, i_o_weight, i_c_weight, pr_o_weight, pr_m_weight, pr_c_weight)
+    df = process_data(df, num_days)
 
-    fig = create_figure(df, log)
+    fig = create_figure(df, num_days)
 
     logging.warning(f"{VIZ_ID} - END - {time.perf_counter() - start}")
     return fig
 
 
-def process_data(
-    df: pd.DataFrame,
-    start_date,
-    end_date,
-    i_o_weight,
-    i_c_weight,
-    pr_o_weight,
-    pr_m_weight,
-    pr_c_weight,
-):
+def process_data(df: pd.DataFrame, num_days):
 
     # convert to datetime objects rather than strings
-    df["created_at"] = pd.to_datetime(df["created_at"], utc=True)
+    df["msg_timestamp"] = pd.to_datetime(df["msg_timestamp"], utc=True)
+    df["pr_created_at"] = pd.to_datetime(df["pr_created_at"], utc=True)
+    df["pr_closed_at"] = pd.to_datetime(df["pr_closed_at"], utc=True)
 
-    # order values chronologically by COLUMN_TO_SORT_BY date
-    df = df.sort_values(by="created_at", axis=0, ascending=True)
+    # drop messages from the pr creator
+    df = df[df["cntrb_id"] != df["msg_cntrb_id"]]
 
-    # filter values based on date picker
-    if start_date is not None:
-        df = df[df.created_at >= start_date]
-    if end_date is not None:
-        df = df[df.created_at <= end_date]
+    # sort in ascending earlier and only get ealiest value
+    df = df.sort_values(by="msg_timestamp", axis=0, ascending=True)
+    df = df.drop_duplicates(subset="pull_request_id", keep="first")
 
-    # df to hold value of unique contributors for each repo
-    df_cntrbs = pd.DataFrame(df.groupby("repo_name")["cntrb_id"].nunique()).rename(
-        columns={"cntrb_id": "num_unique_contributors"}
+    # first and last elements of the dataframe are the
+    # earliest and latest events respectively
+    earliest = df["pr_created_at"].min()
+    latest = max(df["pr_created_at"].max(), df["pr_closed_at"].max())
+
+    # beginning to the end of time by the specified interval
+    dates = pd.date_range(start=earliest, end=latest, freq="D", inclusive="both")
+
+    # df for open prs and responded to prs in time interval
+    df_pr_responses = dates.to_frame(index=False, name="Date")
+
+    # every day, count the number of PRs that are open on that day and the number of
+    # those that were responded to within num_days of their opening
+    df_pr_responses["Open"], df_pr_responses["Response"] = zip(
+        *df_pr_responses.apply(
+            lambda row: get_open_response(df, row.Date, num_days),
+            axis=1,
+        )
     )
 
-    # group actions and repos to get the counts of the actions by repo
-    df_actions = pd.DataFrame(df.groupby("repo_name")["Action"].value_counts())
-    df_actions = df_actions.rename(columns={"Action": "count"}).reset_index()
+    df_pr_responses["Date"] = df_pr_responses["Date"].dt.strftime("%Y-%m-%d")
 
-    # pivot df to reformat the actions to be columns and repo_id to be rows
-    df_actions = df_actions.pivot(index="repo_name", columns="Action", values="count")
+    return df_pr_responses
 
-    # df_consolidated combines the actions and unique contributors and then specific columns for visualization use are added on
-    df_consolidated = pd.concat([df_actions, df_cntrbs], axis=1).reset_index()
 
-    # log of commits and contribs
-    df_consolidated["log_num_commits"] = df_consolidated["Commit"].apply(math.log)
-    df_consolidated["log_num_contrib"] = df_consolidated["num_unique_contributors"].apply(math.log)
+def create_figure(df: pd.DataFrame, num_days):
 
-    # column to hold the weighted values of pr and issues actions summed together
-    df_consolidated["prs_issues_actions_weighted"] = (
-        df_consolidated["Issue Opened"] * i_o_weight
-        + df_consolidated["Issue Closed"] * i_c_weight
-        + df_consolidated["PR Opened"] * pr_o_weight
-        + df_consolidated["PR Merged"] * pr_m_weight
-        + df_consolidated["PR Closed"] * pr_c_weight
+    fig = go.Figure(
+        [
+            go.Scatter(
+                name="Issues open",
+                x=df["Date"],
+                y=df["Open"],
+                mode="lines",
+                showlegend=True,
+                hovertemplate="Issues Open: %{y}<br>%{x|%b %d, %Y} <extra></extra>",
+                marker=dict(color=color_seq[1]),
+            ),
+            go.Scatter(
+                name="Response <" + str(num_days) + " days",
+                x=df["Date"],
+                y=df["Response"],
+                mode="lines",
+                showlegend=True,
+                hovertemplate="PRs: %{y}<br>%{x|%b %d, %Y} <extra></extra>",
+                marker=dict(color=color_seq[5]),
+            ),
+        ]
     )
-
-    # column for log value of pr and issue actions
-    df_consolidated["log_prs_issues_actions_weighted"] = df_consolidated["prs_issues_actions_weighted"].apply(math.log)
-
-    return df_consolidated
-
-
-def create_figure(df: pd.DataFrame, log):
-
-    y_axis = "prs_issues_actions_weighted"
-    y_title = "Weighted PR/Issue Actions"
-    if log:
-        y_axis = "log_prs_issues_actions_weighted"
-        y_title = "Log of Weighted PR/Issue Actions"
-
-    # graph generation
-    fig = px.scatter(
-        df,
-        x="log_num_commits",
-        y=y_axis,
-        color="repo_name",
-        size="log_num_contrib",
-        hover_data=["repo_name", "Commit", "PR Opened", "Issue Opened", "num_unique_contributors"],
-        color_discrete_sequence=color_seq,
-    )
-
-    fig.update_traces(
-        hovertemplate="Repo: %{customdata[0]} <br>Commits: %{customdata[1]} <br>Total PRs: %{customdata[2]}"
-        + "<br>Total Issues: %{customdata[3]} <br>Total Contributors: %{customdata[4]}<br><extra></extra>",
-    )
-
-    # layout styling
     fig.update_layout(
-        xaxis_title="Logarithmic Commits",
-        yaxis_title=y_title,
-        margin_b=40,
+        xaxis_title="Time",
+        yaxis_title="Number of PRs",
         font=dict(size=14),
-        legend_title="Repo Name",
     )
 
     return fig
+
+
+def get_open_response(df, date, num_days):
+    """
+    This function takes a date and determines how many
+    prs in that time interval are opened and if they have a response within num_days.
+
+    Args:
+    -----
+        df : Pandas Dataframe
+            Dataframe with pr assignment actions of the assignees
+
+        date : Datetime Timestamp
+            Timestamp of the date
+
+        num_days : int
+            number of days that a response should be within
+
+    Returns:
+    --------
+        int, int: Number of opened and responded to prs within num_days on the day
+    """
+    # drop rows that are more recent than the date limit
+    df_created = df[df["pr_created_at"] <= date]
+
+    # drops rows that have been closed after date
+    df_open = df_created[df_created["pr_closed_at"] > date]
+
+    # include prs that have not been close yet
+    df_open = pd.concat([df_open, df_created[df_created.pr_closed_at.isnull()]])
+
+    # column to hold date num_days after the pr_creation date for comparision
+    df_open["response_by"] = df_open["pr_created_at"] + pd.DateOffset(days=num_days)
+
+    # Inlcude only the prs that msg timestamp is before the responded by time
+    df_response = df_open[df_open["msg_timestamp"] < df_open["response_by"]]
+
+    # generates number of columns ie open prs
+    num_open = df_open.shape[0]
+
+    # number of prs that had response in time interval
+    num_response = df_response.shape[0]
+
+    return num_open, num_response
